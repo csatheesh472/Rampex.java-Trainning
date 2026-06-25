5edfae2819f55ff3acff570f68bedf6ae1360fdd
sathe <satheeshpriya354@gmail.com>
2026-06-25 14:20:10 +0530

    project created

---
+++ src/week.java
@@ -0,0 +1,14 @@
+public class week {
+    public static void main(String[] args) {
+
+        for (int month=1;month<=12;month++) {
+            for (int week=1;week<=4;week++) {
+                for (int day=1;day<=7;day++) {
+                    System.out.println( " month " + month + " week "+ week +" day "+ day );
+                    System.out.println(" Come to college " +" study and go home ");
+                }
+            }
+        }
+
+    }
+}
